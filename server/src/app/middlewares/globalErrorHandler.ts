ac794cc22f272d0ea67cb1dd82b3afeaaabcc155
muhammad-jiku <muhammadjiku364@gmail.com>
2024-12-27 23:32:44 +0600

    global error handler middleware added

---
+++ server/src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,48 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import config from '../../config';
+import ApiError from '../../errors/ApiError';
+import { handleCastError } from '../../errors/handleCastError';
+import { handleValidationError } from '../../errors/handleValidationError';
+import { IGenericErrorMessage } from '../../interfaces/error';
+import { errorlogger } from '../../shared/logger';
+
+export const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  config?.env === 'development'
+    ? console.log('Global development error handler:', { err })
+    : errorlogger.error('Global production error handler:', err);
+
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorMessages: IGenericErrorMessage[] = [];
+
+  if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (err instanceof ApiError) {
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorMessages = err?.message ? [{ path: '', message: err?.message }] : [];
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorMessages = err?.message ? [{ path: '', message: err?.message }] : [];
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  });
+};
