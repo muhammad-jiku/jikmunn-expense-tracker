03f538c5f32aa0fbac1710d79560f2b4e59ae7b1
muhammad-jiku <muhammadjiku364@gmail.com>
2024-12-28 00:24:23 +0600

    db connection setup and app file added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,48 @@
-import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorlogger, logger } from './shared/logger';
 
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT || 8080;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+process.on('uncaughtException', (error) => {
+  errorlogger.error(error);
+  process.exit(1);
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+let server: Server;
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    logger.info(`Database is connected successfully!`);
+
+    server = app.listen(config.port, () => {
+      logger.info(
+        `[server]: Server is running at http://localhost:${config?.port}`
+      );
+    });
+  } catch (err) {
+    errorlogger.error('Failed to connect database', err);
+  }
+
+  process.on('unhandledRejection', (error) => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+
+bootstrap();
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
 });
